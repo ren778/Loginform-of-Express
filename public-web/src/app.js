b4c7d1c9701d2fcbd8ad2de83c3506cb518d6ee3
ren <0806renkun@gmail.com>
2023-01-01 13:33:19 +0900

    git

--- public-web/src/app.js
+++ public-web/src/app.js
@@ -8,7 +8,6 @@ const io = require('socket.io')(server);
 app.use(express.static(path.join(__dirname, 'web')));
 
 io.on('connection', (socket) => {
-  console.log('Connected');
 });
 
 server.listen(8080, () => console.log('Listened'));
